178cb1e20b1e8906cf776f56046cb33f3ee6c170
lindor03 <lindor.morina@progtelx.com>
2026-03-03 14:48:32 +0100

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,10 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-    'https://ebaa-front.laratest-app.com',
-    'https://e-baa.test',],
-
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
@@ -32,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ];
